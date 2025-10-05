09c33e0f47eaa57b93b56223d82c22ff20a553d9
Davi Monteiro <davi321522@gmail.com>
2025-10-05 15:49:51 -0300

    teste-logout-is-ready

---
+++ cypress/e2e/logout-do-sistema.cy.js
@@ -0,0 +1,16 @@
+describe('Logout bem-sucedido', () => {
+        beforeEach(() => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+        cy.login('Admin','admin123');
+    });
+
+    it('Logado no sistema, clickar botão de logout, devo ser redirecionado para a tela de login', () => {
+        cy.get('.oxd-userdropdown-tab').click();
+        cy.contains('.oxd-userdropdown-link', 'Logout').should('be.visible').click();
+        cy.contains('button', 'Login').should('be.visible');
+
+
+    });
+
+
+});
